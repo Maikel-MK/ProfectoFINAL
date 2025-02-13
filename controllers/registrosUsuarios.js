669734a079a78e842a119da0da6786804781b902
Maikel <maikelalejandro2008@gmail.com>
2025-02-13 16:45:35 -0400

    'base de datos registro correcto'

--- controllers/registrosUsuarios.js
+++ controllers/registrosUsuarios.js
@@ -1,18 +1,36 @@
 const { request, response } = require('../app');
 
 const usersRouter = require('express').Router();
+const User = require('../models/registroUsuario')
 
 // router: CRUD
 
 
 //registrar lo que envia el usuario
-usersRouter.post('/',(request,response)=>{
+usersRouter.post('/registroUsuarios',(request,response)=>{
     const {nombre,correo,password,password2} = request.body
     console.log(nombre,correo,password,password2)
 
     if(!nombre || !correo || !password || !password2){
+
         return response.status(400).json({error:'Los datos no pueden estar Incompletos'})
     }else{
+        //guardar en la bd
+        let usuario = new User()
+
+        usuario.nombre = nombre
+        usuario.correo = correo 
+        usuario.password = password
+        
+
+        async function guardarUsuario() {
+            await usuario.save()
+            const usuarios = await User.find()
+            console.log(usuarios)
+        }
+
+        guardarUsuario().catch(console.error)
+
         return response.status(200).json({message:'Usuario Registrado Correctamente'})
     }
 
