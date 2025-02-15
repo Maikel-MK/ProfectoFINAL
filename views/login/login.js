a2cb53f013c11d60a4aebca511b73ab9f398cb35
Maikel Mendez <maikelalejandro2008@gmail.com>
2025-02-15 01:12:54 -0400

    chapa y pintura home

--- views/login/login.js
+++ views/login/login.js
@@ -20,14 +20,17 @@ formulario.addEventListener('submit', async (e) => {
         // Verifica si la respuesta es un arreglo
         if (Array.isArray(response.data)) {
             listaUsuarios = response.data;
+            console.log('Arreglo de Lista de usuarios:');
         }
         // Verifica si la respuesta es un objeto con una propiedad "data"
         else if (response.data.data && Array.isArray(response.data.data)) {
             listaUsuarios = response.data.data;
+            console.log('Objeto tipo data de Lista de usuarios:');
         }
         // Verifica si la respuesta es un objeto con usuarios individuales
         else if (typeof response.data === 'object') {
             listaUsuarios = Object.values(response.data);
+            console.log('Objeto de usuarios individuales:');
         }
         // Si no es ninguna de las anteriores, lanza un error
         else {
@@ -50,9 +53,22 @@ formulario.addEventListener('submit', async (e) => {
             } else if (usuario.rol === 'usuario') {
                 console.log('Redirigiendo a /clientes/');
                 window.location.href = '/clientes/';
+            } else if (usuario.rol === 'contador') {
+                console.log('Redirigiendo a /contador/');
+                window.location.href = '/contador/';
+            } else if (usuario.rol === 'residente') {
+                console.log('Redirigiendo a /residente/');
+                window.location.href = '/residente/';
+            } else {
+                console.log('Redirigiendo a /');
+                window.location.href = '/';
+                alert(`Usuario sin rol expecifico, ${usuario.correo}!`);
+
             }
+        
         } else {
             alert('Credenciales incorrectas.');
+            window.location.href = '/'
         }
     } catch (error) {
         console.error('Error:', error);
